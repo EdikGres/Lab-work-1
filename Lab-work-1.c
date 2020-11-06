9b222d08d2bb2d620ff44f595ba8d8a621e43da9
Ebuardo <edik_gres@icloud.com>
2020-11-06 13:10:31 +0300

    long long

--- Lab-work-1.c
+++ Lab-work-1.c
@@ -2,34 +2,37 @@
 #include <stdio.h>
 #include <locale.h>
 
+
+static long long factorial(int n);
+
 int main() {
 	char* locale = setlocale(LC_ALL, ""); //Локализация русского
 
 	int n, k;
 
-	printf("Здравстуйте! У меня 8 вариант: \n\"Ввести целые числа N и K. Вычислить и вывести число сочетаний из N по K.\"\n");
-	printf("Введите первое число: ");
+	//printf("Здравстуйте! У меня 8 вариант: \n\"Ввести целые числа N и K. Вычислить и вывести число сочетаний из N по K.\"\n");
+	printf("input N: ");
 	scanf("%d", &n);
-	printf("Введите второе число: ");
+	printf("input K: ");
 	scanf("%d", &k);
 	long result = factorial(n) / ( factorial(n - k) * factorial(k) ); // Формула для нахождения сочетаний
-	printf("Итого сочетаний: %d", result);
+	printf("total combinations: %d", result);
 
 
 	return result;
 }
 
- static long factorial(int n)
+ /*static long factorial(int n)
 {
 	//Использую рекуррентную формулу и тернарное условие для 1
 	return (n < 2) ? 1 : n * factorial(n - 1); 
-} 
-/* static long factorial(int n)
+} */
+ static long long factorial(int n)
 {
-	int r;
+	long long r;
 	for (r = 1; n > 1; r *= (n--));
 	return r;
-} */
+} 
 /*static int factorial_1(int n, int p)
 {
 	return (n < 2) ? p : factorial_1(n - 1, p * n);
